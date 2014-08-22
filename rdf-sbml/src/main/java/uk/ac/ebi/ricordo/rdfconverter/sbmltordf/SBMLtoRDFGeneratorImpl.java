264f68f810c4118594e67226801f5553b78c46e2
Mihai Glonț <mglont@ebi.ac.uk>
2014-08-22 15:50:39 +0100

    minor refactoring

--- rdf-sbml/src/main/java/uk/ac/ebi/ricordo/rdfconverter/sbmltordf/SBMLtoRDFGeneratorImpl.java
+++ rdf-sbml/src/main/java/uk/ac/ebi/ricordo/rdfconverter/sbmltordf/SBMLtoRDFGeneratorImpl.java
@@ -21,8 +21,7 @@ import uk.ac.ebi.biomodels.ws.BioModelsWSClient;
 import uk.ac.ebi.biomodels.ws.BioModelsWSException;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Calendar;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,6 +34,7 @@ public class SBMLtoRDFGeneratorImpl implements SBMLtoRDFGenerator {
     private final BioModelsWSClient client;
     private final SBMLtoRDFCreatorImpl sbmLtoRDFGenerator;
     Logger logger = Logger.getLogger(SBMLtoRDFGeneratorImpl.class);
+    private final boolean IS_INFO_ENABLED = logger.isInfoEnabled();
 
     public SBMLtoRDFGeneratorImpl(BioModelsWSClient client, SBMLtoRDFCreatorImpl sbmLtoRDFGenerator) {
         this.client = client;
@@ -76,19 +76,35 @@ public class SBMLtoRDFGeneratorImpl implements SBMLtoRDFGenerator {
 
     private void allBioModelsFromFolderToRDF(String folderPath) {
         File folder = new File(folderPath);
-        if(folder.isDirectory()){
-            ArrayList<File> files = new ArrayList<File>(Arrays.asList(folder.listFiles()));
-            for(File file:files){
+        if(folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files.length == 0) {
+                logger.info("No SBML/XML files found in this folder " + folderPath);
+                return;
+            }
+            if (IS_INFO_ENABLED) {
+                String msg = String.format("%s\t%s %4d %s.", Calendar.getInstance().getTime().
+                        toString(), "Started processing", files.length, "models.");
+                logger.info(msg);
+            }
+            for(final File file: files) {
                 if(file.isFile()) {
-                    String modelId = file.getName().substring(0, file.getName().indexOf("."));
-                    logger.info("Converting to RDF: " + modelId);
+                    final String modelId = file.getName().split("\\.")[0];
+                    if (IS_INFO_ENABLED) {
+                        logger.info("Converting to RDF: " + modelId);
+                    }
                     sbmLtoRDFGenerator.generateSBMLtoRDFFromFile(modelId, file);
-                }
-                else{
-                    logger.info("No SBML/XML files found in this folder " + folderPath);
+                    if (IS_INFO_ENABLED) {
+                        logger.info(String.format("...Completed %s", modelId));
+                    }
                 }
             }
-        }else{
+            if (IS_INFO_ENABLED) {
+                String msg = String.format("%s\t%s.",
+                        Calendar.getInstance().getTime().toString(), "Finished");
+                logger.info(msg);
+            }
+        } else {
             logger.info("Path not found: " + folderPath);
         }
     }
@@ -113,9 +129,8 @@ public class SBMLtoRDFGeneratorImpl implements SBMLtoRDFGenerator {
 
      private void bioModelsReleaseSetUp(String folderPath, String fileNamePattern, boolean batch) {
         File folder = new File(folderPath);
-        String [] contentlist = folder.list();
-        for(int i =0; i<contentlist.length; i++){
-            File content = new File(folder.getPath()+File.separator+contentlist[i]);
+        File[] contentlist = folder.listFiles();
+        for(File content: contentlist){
             if(content.isDirectory()){
                 bioModelsReleaseSetUp(content.getPath(), fileNamePattern, batch);
             }
@@ -132,20 +147,20 @@ public class SBMLtoRDFGeneratorImpl implements SBMLtoRDFGenerator {
 
     public void pathToModelSetUp(String folderPath, String fileNamePattern) {
         File folder = new File(folderPath);
-        String [] contentlist = folder.list();
-        for(int i =0; i<contentlist.length; i++){
-            File content = new File(folder.getPath()+File.separator+contentlist[i]);
-            if(content.isDirectory()){
-                pathToModelSetUp(content.getPath(), fileNamePattern);
+        File[] contentlist = folder.listFiles();
+        for(File f: contentlist){
+            if(f.isDirectory()){
+                pathToModelSetUp(f.getPath(), fileNamePattern);
             }
-            else if (content.isFile()){
-                if(content.getName().matches(fileNamePattern)){
+            else if (f.isFile()){
+                if(f.getName().matches(fileNamePattern)){
                     logger.info("Converting to RDF: " + folder.getName());
                     String outputFolder = sbmLtoRDFGenerator.getOutputFolder();
-                    String tempOutputFolder = outputFolder + content.getParentFile().getParentFile().getName() + File.separator;
+                    String tempOutputFolder = outputFolder + f.getParentFile().getParentFile().
+                            getName() + File.separator;
                     new File(tempOutputFolder).mkdir();
                     sbmLtoRDFGenerator.setOutputFolder(tempOutputFolder);
-                    sbmLtoRDFGenerator.generateSBMLtoRDFFromFile(folder.getName(), content);
+                    sbmLtoRDFGenerator.generateSBMLtoRDFFromFile(folder.getName(), f);
                     sbmLtoRDFGenerator.setOutputFolder(outputFolder);
                 }
             }
