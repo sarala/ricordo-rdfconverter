ef73e24e5eb6cf0d7306a13f0206f4ea9c855651
sarala <sarala.dissanayake@gmail.com>
2016-05-05 15:33:37 +0100

    updated to jsbml 1.1

--- rdf-sbml/src/main/java/uk/ac/ebi/ricordo/rdfconverter/SBMLMain.java
+++ rdf-sbml/src/main/java/uk/ac/ebi/ricordo/rdfconverter/SBMLMain.java
@@ -20,7 +20,7 @@ public class SBMLMain {
 //        sbmlToRdfGenerator.allNonCuratedModelsFromBioModelsDBToRDF();
 //        sbmlToRdfGenerator.allModelsFromBioModelsDBToRDF();
 //          sbmlToRdfGenerator.aModelFromBioModelsDBToRDF("BIOMD0000000063");
-//        sbmlToRdfGenerator.allBioModelsFromFolderToRDF("/Users/sarala/Downloads/noncura/");
+  //      sbmlToRdfGenerator.allBioModelsFromFolderToRDF("/Users/sarala/Downloads/noncura/");
 //        sbmlToRdfGenerator.aBioModelFromFileToRDF("/Users/sarala/Downloads/MODEL1507180026.xml");
 //        sbmlToRdfGenerator.bioModelsReleaseSetUp("C:\\Users\\sarala.EBI\\Documents\\GitHub\\ricordo-rdfconverter\\resources\\sbmlfoldertest\\", "(BIOMD|MODEL)\\d{10}"+".xml");
 //        sbmlToRdfGenerator.pathToModelSetUp("/nfs/production/biomodels/biomodels/releases/r24/metabolic/", "(BIOMD|MODEL|BMID)\\d{12}"+"_url.xml");
