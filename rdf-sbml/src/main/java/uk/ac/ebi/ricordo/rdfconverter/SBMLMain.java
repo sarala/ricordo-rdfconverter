cc65070c43be994c583c09cdf078879920bde20a
sarala <sarala.dissanayake@gmail.com>
2016-05-10 11:30:40 +0100

    updated sbml provenance

--- rdf-sbml/src/main/java/uk/ac/ebi/ricordo/rdfconverter/SBMLMain.java
+++ rdf-sbml/src/main/java/uk/ac/ebi/ricordo/rdfconverter/SBMLMain.java
@@ -21,7 +21,7 @@ public class SBMLMain {
 //        sbmlToRdfGenerator.allModelsFromBioModelsDBToRDF();
 //          sbmlToRdfGenerator.aModelFromBioModelsDBToRDF("BIOMD0000000063");
   //      sbmlToRdfGenerator.allBioModelsFromFolderToRDF("/Users/sarala/Downloads/noncura/");
-//        sbmlToRdfGenerator.aBioModelFromFileToRDF("/Users/sarala/Downloads/MODEL1507180026.xml");
+        sbmlToRdfGenerator.aBioModelFromFileToRDF("/Users/sarala/Downloads/MODEL1112110002.xml");
 //        sbmlToRdfGenerator.bioModelsReleaseSetUp("C:\\Users\\sarala.EBI\\Documents\\GitHub\\ricordo-rdfconverter\\resources\\sbmlfoldertest\\", "(BIOMD|MODEL)\\d{10}"+".xml");
 //        sbmlToRdfGenerator.pathToModelSetUp("/nfs/production/biomodels/biomodels/releases/r24/metabolic/", "(BIOMD|MODEL|BMID)\\d{12}"+"_url.xml");
 
